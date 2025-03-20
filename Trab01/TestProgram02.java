196bc758b9c31c243f964157d6fe3c2366789a26
Cinthia Blanco Rodrigo <cblancor@ulasalle.edu.pe>
2025-03-20 13:49:15 -0500

    Scanner , java clonación

---
+++ Trab01/TestProgram02.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class TestProgram02{
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+		double x = scanner.nextDouble();
+		double y = scanner.nextDouble();
+		scanner.close();
+    }
+}
